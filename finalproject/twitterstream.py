1cc58a3945df12291d0447d8912ac8afa2db9fc6
rmauriello <jzzlvr@hotmail.com>
2013-07-22 00:04:58 -0400

    reduced tokens

--- finalproject/twitterstream.py
+++ finalproject/twitterstream.py
@@ -2,19 +2,41 @@
 # Use OAUTH to access Twitter's live 1% stream of tweets
 #  Write to STDOUT
 #
+# Everything works but if I pass location parameter, get error message
+#
+
+import sys
+import json
+import string, re
+
 import oauth2 as oauth
 import urllib2 as urllib
 import sys
+import pandas as pd
+
+def filter_tweets(text):
+    filter(lambda x: x in string.printable, line)
+    if re.match(r'^{"delete".*',line):
+        deleted_tweets += 1
+        next
+    else:
+        data.append(json.loads(line))
 
 
 #
 # Get authorization using OAUTH. 
 #
-access_token_key    = "1408486478-YLXMB9oFiVYDMItD8iV5pGCp2XWJbh7vnKXNcjx"
-access_token_secret = "MPDV3m2NJBRfspUp8YY3kpphp0kKMOeOJ5QLxjWb5s"
 
-consumer_key        = "tU7FuiUiVNwTW03Yj11giA"
-consumer_secret     = "hgTgyehPDlQeR9p5PuYMemkk5ttd62j2cb3qqcQxK8"
+try:
+  tokens = pd.read_csv("Data/twitter_tokens.txt", header=None)
+except:
+  print "Error reading tokens file"
+
+access_token_key    = 
+access_token_secret = 
+
+consumer_key        = 
+consumer_secret     = 
 
 _debug = 0
 
@@ -33,13 +55,21 @@ https_handler = urllib.HTTPSHandler(debuglevel=_debug)
 # Construct, sign, and open a twitter request using the hard-coded credentials above.
 #
 def twitterreq(url, method, parameters):
-  print "twitterreq ", parameters
-  req = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                             token=oauth_token,
-                                             http_method=http_method,
-                                             http_url=url, 
-                                             parameters=parameters)
-  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+  try:
+    req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                               token=oauth_token,
+                                               http_method=http_method,
+                                               http_url=url, 
+                                              parameters=parameters)
+  except:
+    e = sys.exc_info()[0]
+    print "Error - req = ", req, "\n", e
+
+  try:
+    req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+  except:
+    e = sys.exc_info()[0]
+    print "Error - req_sign = ", req_sign, "\n", e
 
   headers = req.to_header()
 
@@ -49,7 +79,6 @@ def twitterreq(url, method, parameters):
     encoded_post_data = None
     url = req.to_url()
 
-
   opener = urllib.OpenerDirector()
   opener.add_handler(http_handler)
   opener.add_handler(https_handler)
@@ -58,17 +87,20 @@ def twitterreq(url, method, parameters):
   return response
 
 #
-# Bounding box for contiguous US is:
-
+# Bounding box for contiguous US is:   '-124.7625,24.5210,-66.9326,49.3845'
+#
 def fetchsamples():
-  url = "https://stream.twitter.com/1/statuses/sample.json"
+  # url = "https://stream.twitter.com/1/statuses/sample.json"
+  url = "https://stream.twitter.com/1/statuses/filter.json"
   parameters={}
   parameters['locations'] = '-124.7625,24.5210,-66.9326,49.3845'
+  # parameters['locations']="-122.75,36.8,-121.75,37.8" # Longitude/Latitude for San Francisco box
   
   try:
     response = twitterreq(url, "GET", parameters)
     for line in response:
       print line.strip()
+      # - filtering here ..self.
   except:
     e = sys.exc_info()[0]
     print "Error with twitter request", url, "GET", parameters
