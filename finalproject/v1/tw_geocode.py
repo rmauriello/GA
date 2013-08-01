75d055bdcc8039eed3d321cabfc1104f8e41ef81
rmauriello <jzzlvr@hotmail.com>
2013-08-01 01:24:44 -0400

    updated spatial Rtree code

--- finalproject/v1/tw_geocode.py
+++ finalproject/v1/tw_geocode.py
@@ -1,59 +1,97 @@
+#!/usr/bin/env python
 
+import sys
 import shapely
 import shapefile
-import sys
+from shapely.geometry import Point, Polygon
 from rtree import index
+from math import log
+
+
+# 42.66326054 -87.80922716 [3180, 3193] - either kenosha or racine county. 
 
 #
 # Given latitude/longitude, return the FIPS county code
+#  Some rtree intersection logic from https://gist.github.com/fawcett/5251327
 #
 def latlon2countycode(lat,lon):
 	try:
 		hits = list(idx.intersection((lon,lat,lon,lat)))
-		recno = hits[0]
-		# print lat,lon, recno, records[recno][5],  records[recno][7]
-		# Do more testing here in case there are multiple matches
+		if len(hits) == 1:
+			recno = hits[0]
+		elif len(hits) > 1:
+			# print "multiple hits: ", lat, lon, hits
+			for hitIdx in hits:
+				county = shapes[hitIdx]  # county needs to be polygon?
+				poly   = Polygon(county.points)
+				if poly.contains(Point(lon,lat)):
+					# print "\t\t found ", hitIdx, records[hitIdx][5], records[hitIdx][7]
+					recno = int(hitIdx)
+					break
+		else:
+			recno = -1
 	except:
-		pass
-	return recno
+		recno = -1
+	return int(recno)
 
-print "reading shapefiles and making rtree index"
-sf=shapefile.Reader("../Data/shapefiles/uscounties/all_counties.shp")
-
-shapes  = sf.shapes()    # to get the polygons in the shapefiles
-records = sf.records()   # to get the data, particulary county name and FIPS code
+# print "#Reading shapefiles and making rtree index"
+try:
+	sf      = shapefile.Reader("../Data/shapefiles/uscounties/all_counties.shp")
+	shapes  = sf.shapes()    # Get the polygons in the shapefiles
+	records = sf.records()   # Get the data, particulary county name and FIPS code
+except:
+	# print "#Error reading shapefiles. Exiting..."
+	exit(-1)
 
 # Create rtree index and load the bounding boxes from the shapefiles
-#
 idx = index.Index()
 for i, shape in enumerate(shapes):
     idx.insert(i, shape.bbox)
 
-print "Starting to parse input"
+# print "#Starting to parse input"
 # Initialize county aggregator
 county_sum = {}
 
 for line in sys.stdin:
-	words = line.split(',')
-	# print "parsing line...", words[0],"EOL"
 	
 	try:
+		words = line.split(',')
+		# print "parsing line...", words[0],"EOL"
+
 		lat = float(words[0].strip()[2:])
 		lon = float(words[1].strip()[0:-1])
 
 		recno = latlon2countycode(lat,lon)
-		county_code = records[recno][5]
-		county_name = records[recno][7]
-		# Set default (1) otherwise increment count
+		if recno > 0:
+			county_code = records[recno][5]
+			county_name = records[recno][7]
 
-		county_sum[county_code] = county_sum.get(county_code, 0) + 1
+			# Set default (1) otherwise increment count
+			# 	
+			county_sum[county_code] = county_sum.get(county_code, 0) + 1
+			# try:
+			# 	county_sum[county_code] += 1
+			# except KeyError:
+			# 	county_sum[county_code] = 1
 
+			# print recno, county_code, county_name
+		else:
+			# print "Error with recno"
+			continue
 	except:
 		continue
 
+#
+# Scale the county data by maximum value.
+#
+# Need to find 2010 Census population by FIPS county dataset
+#
 
+
+max_sum = max(county_sum.values())
+print "id\trate"             # d3.tsv wants a header line by default
 for key,value in county_sum.items():
-	print key, value
+	print "%s\t%0.5f" % (key,  abs(log(float(value)/max_sum))/70.0 )
 
 # if __name__ == '__main__':
 #     main()
