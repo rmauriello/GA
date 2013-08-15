f755b2bc954ee21b63c3fb41d6b3f48280000114
rmauriello <jzzlvr@hotmail.com>
2013-08-14 22:47:26 -0400

    working set of HTML pages

---
+++ finalproject/v1/tw_geo_norm.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+#
+# Input:
+#   Twitter counts (insults, total) by county
+#   Population by county (2010 census)
+# Output:
+#   scaled Location Quotient by FIPS county code
+#
+
+import pandas as pd
+import numpy as np
+
+
+def main():
+	
+	try:
+		pop_county    = pd.read_csv("../Data/fipscounties.csv",sep=",",header=0)
+		tweets_county = pd.read_csv("../Data/tweet_counts.tsv",sep="\t",header=0,names=['code','value'])
+	except:
+		print "Could not open file. Exiting"
+		exit(-1)
+
+
+	# Compare Actual vs. Expected tweets
+
+	j = pd.merge(tweets_county,pop_county, on='code')
+	
+
+	# Let's calculate tweet count, linearly scale to [0,1]
+	# Should correlate to US population
+	#
+	j['tweets'] = j['value']/j['value'].max()
+	min = j['tweets'].min() # 
+	max = j['tweets'].max() # s/b 1
+
+	scale_factor = 1.0/(max - min)
+	j['count'] = (j['tweets'] - min) * scale_factor
+
+	fo = open("twitter.tsv","w")
+	d.to_csv(fo, cols=['code','count'], index=None,sep="\t",float_format="%0.5f")
+	fo.close()
+
+
+	# Let's calculate population ratio and log scale to [0,1]
+	# Should not necessarily correlate to US population
+	#
+	us_pop      = j['pop'].sum()
+	tweet_tot   = j['value'].sum() 
+	tweet_perc  = j['value']/float(tweet_tot)
+	county_perc = j['pop']/float(us_pop)
+
+	# Assume tweets correlate to population - calculate ratio
+	#  - ratio > 1: 
+	#          = 1:
+	#          < 1: 
+
+	j['LQ']     = (tweet_perc/county_perc)
+
+	# Log transform
+	j['LQN']    = np.log(j[j['LQ'] > 0]['LQ'])
+
+
+	# Calculate scale factor
+	min = j['LQN'].min()
+	max = j['LQN'].max()
+	scale_factor = 1.0/(max - min)
+	j['scaled'] = (j['LQN'] - min) * scale_factor
+
+	# Write to normalized file, ignoring the log(0) record
+	d = j[j['LQ'] > 0][['code','scaled']]
+
+	fo = open("twitterN.tsv","w")
+	d.to_csv(fo, cols=['code','scaled'], index=None,sep="\t",float_format="%0.5f")
+	fo.close()
+	
+
+
+if __name__ == '__main__':
+    main()
